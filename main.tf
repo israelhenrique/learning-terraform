a9c6a3b5cbb2b4807b0769e5791a863d42a8be11
Israel Lima <israelhenrique@protonmail.com>
2018-11-05 22:32:11 -0200

    deploy a load balancer

--- main.tf
+++ main.tf
@@ -36,6 +36,9 @@ resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
   availability_zones = ["${data.aws_availability_zones.all.names}"]
 
+  load_balancers = ["${aws_elb.example.id}"]
+  health_check_type = "ELB"
+
   max_size = 10
   min_size = 2
 
@@ -46,6 +49,45 @@ resource "aws_autoscaling_group" "example" {
   }
 }
 
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+    timeout = 3
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 data "aws_availability_zones" "all" {}
 
 variable "server_port" {
@@ -53,6 +95,6 @@ variable "server_port" {
   default = 8080
 }
 
-output "public_ip" {
-  value = "${aws_launch_configuration.example.associate_public_ip_address}"
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
 }
