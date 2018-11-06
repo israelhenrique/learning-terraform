e6aaf64fa4b497cef9fffa93355771b487941f04
Israel Lima <israelhenrique@protonmail.com>
2018-11-05 22:05:04 -0200

    deploy a cluster of web servers

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-40d28157"
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-40d28157"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  security_groups = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -13,8 +13,8 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
-  tags {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -27,7 +27,26 @@ resource "aws_security_group" "instance" {
     to_port = "${var.server_port}"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  max_size = 10
+  min_size = 2
+
+  tag {
+    key = "Name"
+    propagate_at_launch = true
+    value = "terraform-asg-example"
+  }
+}
+
+data "aws_availability_zones" "all" {}
 
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
@@ -35,5 +54,5 @@ variable "server_port" {
 }
 
 output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
+  value = "${aws_launch_configuration.example.associate_public_ip_address}"
 }
